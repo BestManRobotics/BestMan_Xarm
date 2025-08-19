bf199667fedf0bf746d5bd5ddb9d5c1c1b9e75ff
Yongchong Gu <107824980+Tastooger@users.noreply.github.com>
2025-08-20 01:40:28 +0800

    Update move_arm_to_joint_values.py

--- Examples/move_arm_to_joint_values.py
+++ Examples/move_arm_to_joint_values.py
@@ -33,7 +33,7 @@ def main():
         import math
         target_joint = [16.8, -24.1, -24.7, 11.2, -77.7, -5.1]
         target_joint = [math.radians(target_joint[n]) for n in range(6)]
-        bestman.move_arm_to_joint_values(target_joint)
+        bestman.move_to_joint_angles(target_joint)
 
     except Exception as e:
         # Log any exceptions that occur
