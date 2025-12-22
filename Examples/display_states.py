6905287178718a76920f9ea302b8791915d40821
zxzm-zak <104998841+zxzm-zak@users.noreply.github.com>
2025-12-22 20:23:39 +0800

    Create display_states.py

---
+++ Examples/display_states.py
@@ -0,0 +1,36 @@
+
+import argparse
+import time
+import sys
+import os
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(os.path.join(parent_dir, 'RoboticsToolBox'))
+# import pyRobotiqGripper
+from Bestman_real_xarm6 import *
+
+def main():
+    # Parse Arguments
+    argparser = argparse.ArgumentParser(
+        description="Move the robot arm to follow a trajectory."
+    )
+    # Required arguments
+    argparser.add_argument("robot_ip", help="IP address of the robot server")
+    args = argparser.parse_args()
+
+    try:
+        # Instantiate the robot interface
+        bestman = Bestman_Real_Xarm6(args.robot_ip)
+
+        # Initialize robot
+        if not bestman.initialize_robot():
+            return  # Exit if initialization fails
+
+        bestman.update_robot_states()
+
+    except Exception as e:
+        # Log any exceptions that occur
+        print(str(e))
+
+
+if __name__ == "__main__":
+    main()
