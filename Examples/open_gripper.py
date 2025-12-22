a92590acc1b6a0e70696aad58890ed6a3c6ecdd3
zxzm-zak <104998841+zxzm-zak@users.noreply.github.com>
2025-12-22 20:30:36 +0800

    Clean up open_gripper.py by removing comments
    Removed commented-out code related to argument parsing and fault handling.

--- Examples/open_gripper.py
+++ Examples/open_gripper.py
@@ -12,30 +12,17 @@ sys.path.append(os.path.join(parent_dir, 'RoboticsToolBox'))
 from Bestman_real_xarm6 import *
 from time import time, sleep
 import numpy as np
+
 def main():
     # Parse Arguments
     argparser = argparse.ArgumentParser(description="Xarm6.")
     # Required arguments
     argparser.add_argument("robot_ip", help="IP address of the robot server")
-    # argparser.add_argument("local_ip", help="IP address of this PC")
-    # argparser.add_argument("frequency", type=int, help="Command frequency, 1 to 200 [Hz]")
-    # Optional arguments
-    # argparser.add_argument("--hold", action="store_true", help="Robot holds current joint positions, otherwise do a sine-sweep")
     args = argparser.parse_args()
 
     try:
         # Instantiate the robot interface
         bestman = Bestman_Real_Xarm6(args.robot_ip, None, None)
-
-        # Clear fault on the robot server if any
-        # if bestman.robot.isFault():
-        #     log.warn("Fault occurred on the robot server, trying to clear ...")
-        #     bestman.robot.clearFault()
-        #     time.sleep(2)
-        #     if bestman.robot.isFault():
-        #         log.error("Fault cannot be cleared, exiting ...")
-        #         return
-        #     log.info("Fault on the robot server is cleared")
 
         # open gripper
         bestman.open_gripper_xarm()
